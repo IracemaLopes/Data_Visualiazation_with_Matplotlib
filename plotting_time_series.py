4bd1dfe8dd3eaef4ea5364159d590dd71c4bd095
IracemaLopes <87488647+IracemaLopes@users.noreply.github.com>
2021-09-12 17:01:10 +0100

    Update plotting_time_series.py

--- plotting_time_series.py
+++ plotting_time_series.py
@@ -5,6 +5,20 @@ fig, ax = plt.subplots()
 # Read the data from file using read_csv
 climate_change = pd.read_csv("climate_change.csv", parse_dates=["date"], index_col=["date"])
 
+# Define a function called plot_timeseries
+def plot_timeseries(axes, x, y, color, xlabel, ylabel):
+
+  # Plot the inputs x,y in the provided color
+  axes.plot(x, y, color=color)
+
+  # Set the x-axis label
+  axes.set_xlabel(xlabel)
+
+  # Set the y-axis label
+  axes.set_ylabel(ylabel, color=color)
+
+  # Set the colors tick params for y-axis
+  axes.tick_params('y', colors=color)
 
 
 # Plot the CO2 levels time-series in blue
