8b1cfb4e8d2896069387bad9464dbf0614ef6e28
IracemaLopes <iralopez92@gmail.com>
2021-09-12 16:40:05 +0100

    Added plot_time_series_data.py

---
+++ plot_time_series_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+
+# Read the data from file using read_csv
+climate_change = pd.read_csv("climate_change.csv", parse_dates=["date"], index_col=["date"])
+
+# Add the time-series for "relative_temp" to the plot
+ax.plot(climate_change.index, climate_change["relative_temp"])
+
+# Set the x-axis label
+ax.set_xlabel("Time")
+
+# Set the y-axis label
+ax.set_ylabel("Relative temperature (Celsius)")
+
+# Show the figure
+plt.show()
